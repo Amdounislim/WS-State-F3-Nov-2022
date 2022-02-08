176f76ba2c37db0acdb43ee8f59dbc137dba13c5
Amdounislim <amdouni.slim@gmail.com>
2022-02-08 12:42:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css"
+import AddItem from './Components/AddItem';
+import Counter from './Components/Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     title: "WS-state"
+  //   }
+  // }
+
+  state = {
+    title: "WS-state",
+    age: 25,
+    obj: {
+      name: "Moncef",
+      email: "moncef@gmail.com"
+    },
+    arr: ["Bonjour", "Hello", "3aslema"],
+    count: 0
+  }
+
+
+  add=(x)=>{
+    this.setState({
+      arr:[...this.state.arr, x]
+    })
+  }
+
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
+
+  render() {
+    return (<div className='App'>
+      {/* <h1>{this.state.title}</h1>
+      <h3>{this.state.age}</h3>
+      <h2>{this.state.obj.name}</h2>
+      <div>{this.state.arr.map(el => <h3>{el}</h3>)}</div> */}
+
+      {/* <button onClick={this.increment}>+</button> */}
+      {/* <h1>{this.state.count}</h1> */}
+      {/* <Counter count={this.state.count} />
+      <button onClick={this.decrement} >-</button> */}
+
+      <AddItem add={this.add} />
+      <div>{this.state.arr.map(el => <h3>{el}</h3>)}</div>
+    </div>);
+  }
 }
 
-export default App;
---
+++ src/Components/AddItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class AddItem extends Component {
+
+    state = {
+        text: ""
+    }
+
+    render() {
+        return <div>
+            <input type="text" onChange={(e) => this.setState({ text: e.target.value })} />
+            <button onClick={()=>this.props.add(this.state.text)} >ADD</button>
+        </div>;
+    }
+}
